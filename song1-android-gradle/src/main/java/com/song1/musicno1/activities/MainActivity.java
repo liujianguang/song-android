fd5341e521fe69f5d6f550e3f9322a512ca941d6
windless <windlessg@gmail.com>
2014-03-06 11:40:59 +0800

    Add androlog

--- song1-android-gradle/src/main/java/com/song1/musicno1/activities/MainActivity.java
+++ song1-android-gradle/src/main/java/com/song1/musicno1/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import butterknife.ButterKnife;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.song1.musicno1.R;
+import de.akquinet.android.androlog.Log;
 
 /**
  * User: windless
@@ -14,6 +15,8 @@ public class MainActivity extends SherlockFragmentActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Log.init();
+
     setContentView(R.layout.activity_main);
     ButterKnife.inject(this);
   }
