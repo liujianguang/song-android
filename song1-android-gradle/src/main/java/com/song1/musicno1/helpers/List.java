02cf501c92b8ad3d651d64abc9a5c69586c069f8
Abner Zhong <windlessg@gmail.com>
2014-03-27 17:23:41 +0800

    New list for Java 8

--- song1-android-gradle/src/main/java/com/song1/musicno1/helpers/List.java
+++ song1-android-gradle/src/main/java/com/song1/musicno1/helpers/List.java
@@ -1,5 +1,6 @@
 package com.song1.musicno1.helpers;
 
+import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 
 import java.util.ArrayList;
@@ -12,6 +13,21 @@ public class List<E> extends ArrayList<E> {
     return new List<>();
   }
 
+  public List<E> each(Block<E> block) {
+    for (E e : this) {
+      block.call(e);
+    }
+    return this;
+  }
+
+  public <T> List<T> map(Function<E, T> function) {
+    List<T> newList = List.newList();
+    for (E e : this) {
+      newList.add(function.apply(e));
+    }
+    return newList;
+  }
+
   public List<E> deleteIf(Predicate<E> predicate) {
     for (E e : this) {
       if (predicate.apply(e)) {
@@ -20,5 +36,29 @@ public class List<E> extends ArrayList<E> {
       }
     }
     return this;
+  }
+
+  public List<E> select(Predicate<E> predicate) {
+    List<E> newList = List.newList();
+    for (E e : this) {
+      if (predicate.apply(e)) {
+        newList.add(e);
+      }
+    }
+    return newList;
+  }
+
+  public List<E> reject(Predicate<E> predicate) {
+    List<E> newList = List.newList();
+    for (E e : this) {
+      if (!predicate.apply(e)) {
+        newList.add(e);
+      }
+    }
+    return newList;
+  }
+
+  public interface Block<E> {
+    void call(E e);
   }
 }
