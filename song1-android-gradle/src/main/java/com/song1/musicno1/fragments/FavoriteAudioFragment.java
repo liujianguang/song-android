4de59455c08c51e2cd2e261620ec1d0ed1c2c4e5
Abner Zhong <windlessg@gmail.com>
2014-05-28 16:01:26 +0800

    自建歌单新 UI

--- song1-android-gradle/src/main/java/com/song1/musicno1/fragments/FavoriteAudioFragment.java
+++ song1-android-gradle/src/main/java/com/song1/musicno1/fragments/FavoriteAudioFragment.java
@@ -6,6 +6,7 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -13,10 +14,14 @@ import com.song1.musicno1.R;
 import com.song1.musicno1.adapter.DataAdapter;
 import com.song1.musicno1.fragments.base.ListFragment;
 import com.song1.musicno1.helpers.List8;
+import com.song1.musicno1.helpers.MainBus;
 import com.song1.musicno1.models.Favorite;
 import com.song1.musicno1.models.play.Audio;
+import com.song1.musicno1.models.play.Player;
 import com.song1.musicno1.models.play.Players;
 import com.song1.musicno1.models.play.Playlist;
+import com.song1.musicno1.stores.PlayerStore;
+import com.squareup.otto.Subscribe;
 
 import java.util.List;
 
@@ -24,7 +29,8 @@ import java.util.List;
  * Created by windless on 14-4-10.
  */
 public class FavoriteAudioFragment extends ListFragment<Audio> implements AdapterView.OnItemClickListener {
-  private Favorite favorite;
+  private   Favorite favorite;
+  protected Audio    playingAudio;
 
   @Override
   public void onActivityCreated(Bundle savedInstanceState) {
@@ -56,6 +62,28 @@ public class FavoriteAudioFragment extends ListFragment<Audio> implements Adapte
   public void onResume() {
     super.onResume();
     reload();
+    MainBus.register(this);
+    updateCurrentPlayerState(null);
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    MainBus.unregister(this);
+  }
+
+  @Subscribe
+  public void updateCurrentPlayerState(PlayerStore.CurrentPlayerChangedEvent event) {
+    updatePlayingAudio(null);
+  }
+
+  @Subscribe
+  public void updatePlayingAudio(PlayerStore.PlayerPlayingAudioChangedEvent event) {
+    Player currentPlayer = PlayerStore.INSTANCE.getCurrentPlayer();
+    if (currentPlayer != null) {
+      playingAudio = currentPlayer.getPlayingAudio();
+    }
+    getAdapter().notifyDataSetChanged();
   }
 
   @Override
@@ -66,15 +94,26 @@ public class FavoriteAudioFragment extends ListFragment<Audio> implements Adapte
         ViewHolder holder;
 
         if (view == null) {
-          view = View.inflate(getActivity(), R.layout.item_text, null);
+          view = View.inflate(getActivity(), R.layout.item_audio, null);
           holder = new ViewHolder(view);
           view.setTag(holder);
         } else {
           holder = (ViewHolder) view.getTag();
         }
 
+
         Audio audio = getDataItem(i);
+        holder.index.setText("" + (i + 1));
         holder.title.setText(audio.getTitle());
+        holder.subtitle.setText(audio.getSubtitle(context));
+
+        if (audio.isEqual(playingAudio)) {
+          holder.index.setVisibility(View.GONE);
+          holder.playingFlag.setVisibility(View.VISIBLE);
+        } else {
+          holder.index.setVisibility(View.VISIBLE);
+          holder.playingFlag.setVisibility(View.GONE);
+        }
 
         return view;
       }
@@ -92,10 +131,18 @@ public class FavoriteAudioFragment extends ListFragment<Audio> implements Adapte
   }
 
   class ViewHolder {
-    @InjectView(R.id.title) TextView title;
+    @InjectView(R.id.title)        TextView    title;
+    @InjectView(R.id.subtitle)     TextView    subtitle;
+    @InjectView(R.id.menu_btn)     ImageButton menu_btn;
+    @InjectView(R.id.index)        TextView    index;
+    @InjectView(R.id.menu)         View        menu;
+    @InjectView(R.id.playing_flag) View        playingFlag;
 
     public ViewHolder(View view) {
       ButterKnife.inject(this, view);
+      menu.setVisibility(View.GONE);
+      menu_btn.setVisibility(View.GONE);
+      index.setVisibility(View.VISIBLE);
     }
   }
 }
